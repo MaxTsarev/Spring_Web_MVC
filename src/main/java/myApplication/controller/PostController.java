0c6c0056e0598e00e501ed3da24a4c956e95feb7
MaxTsarev <maxtsar33@gmail.com>
2023-12-26 18:04:08 +0300

    PostController correction

--- src/main/java/myApplication/controller/PostController.java
+++ src/main/java/myApplication/controller/PostController.java
@@ -11,8 +11,6 @@ import java.util.concurrent.Executors;
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
-//    private final ExecutorService executor = Executors.newCachedThreadPool();
-    public static final String APPLICATION_JSON = "application/json";
     private final PostService service;
 
     public PostController(PostService service) {
@@ -40,46 +38,4 @@ public class PostController {
     }
 }
 
-//public class PostController {
-//
-//    private final ExecutorService executor = Executors.newCachedThreadPool();
-//    public static final String APPLICATION_JSON = "application/json";
-//    private final PostService service;
-//
-//    public PostController(PostService service) {
-//        this.service = service;
-//    }
-//
-//    public void all(HttpServletResponse response) throws IOException, ExecutionException, InterruptedException {
-//        response.setContentType(APPLICATION_JSON);
-//        final var data = executor.submit(service::all).get();
-//        final var gson = new Gson();
-//        response.getWriter().print(gson.toJson(data));
-//    }
-//
-//    public void getById(long id, HttpServletResponse response) throws IOException, ExecutionException, InterruptedException {
-//        response.setContentType(APPLICATION_JSON);
-//        final var gson = new Gson();
-//        final var content = executor.submit(() -> service.getById(id)).get();
-//        response.getWriter().print(gson.toJson(content));
-//    }
-//
-//    public void save(Reader body, HttpServletResponse response) throws IOException, ExecutionException, InterruptedException {
-//        response.setContentType(APPLICATION_JSON);
-//        final var gson = new Gson();
-//        final var post = gson.fromJson(body, Post.class);
-//        final var data = executor.submit(() -> service.save(post)).get();
-//        if (data == null) {
-//            response.getWriter().print(new NotFoundException("Incorrect id input!"));
-//            return;
-//        }
-//        response.getWriter().print(gson.toJson(data));
-//    }
-//
-//    public void removeById(long id, HttpServletResponse response) throws IOException, InterruptedException, ExecutionException {
-//        response.setContentType(APPLICATION_JSON);
-//        final var gson = new Gson();
-//        executor.submit(() -> service.removeById(id));
-//        response.getWriter().print(gson.toJson(service.all()));
-//    }
-//}
+
